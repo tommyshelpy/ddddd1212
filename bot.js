4226be396447f4120c48f305050e653cb36959b8
tommyshelpy <62201309+tommyshelpy@users.noreply.github.com>
2020-03-15 15:22:54 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,7 +11,7 @@ client.on('ready', () => {
       console.log(`ON ${client.guilds.size} Servers '     Brodcast Bot ' `);
     console.log(`----------------`);
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`$bc |DF_Team`,"http://twitch.tv/Death Shop")
+client.user.setGame(`𝐃𝐚𝐫𝐤 𝐃𝐞𝐯𝐢𝐥 𝐍𝐞𝐭𝐰𝐨𝐫𝐤`,"http://twitch.tv/Death Shop")
 client.user.setStatus("dnd")
 });
 
